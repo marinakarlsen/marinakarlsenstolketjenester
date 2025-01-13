f25507a3290a1823b6f3c0095f5090a2c0362444
marinakarlsen <163747526+marinakarlsen@users.noreply.github.com>
2025-01-13 17:07:46 +0100

    Endring på logo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+const logoContainer = document.querySelector('.logo-container');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        logoContainer.classList.add('shrink');
+    } else {
+        logoContainer.classList.remove('shrink');
+    }
+});
+
+const logoLink = document.querySelector('.logo-container a');
+logoLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
